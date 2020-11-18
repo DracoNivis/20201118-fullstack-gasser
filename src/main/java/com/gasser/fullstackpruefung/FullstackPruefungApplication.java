1187ab1911396b3de48cb515c75029190505c702
draconivis <patzegasser99@gmail.com>
2020-11-18 13:45:00 +0100

    initial commit

---
+++ src/main/java/com/gasser/fullstackpruefung/FullstackPruefungApplication.java
@@ -0,0 +1,13 @@
+package com.gasser.fullstackpruefung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackPruefungApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackPruefungApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/gasser/fullstackpruefung/FullstackPruefungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasser.fullstackpruefung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackPruefungApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
