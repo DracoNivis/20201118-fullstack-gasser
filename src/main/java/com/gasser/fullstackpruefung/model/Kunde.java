16f0bd097f65f1521edb3c71b81eb43a7c3a5725
draconivis <patzegasser99@gmail.com>
2020-11-18 14:03:10 +0100

    updated Kunde

--- src/main/java/com/gasser/fullstackpruefung/model/Kunde.java
+++ src/main/java/com/gasser/fullstackpruefung/model/Kunde.java
@@ -1,4 +1,28 @@
 package com.gasser.fullstackpruefung.model;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.*;
+
+@Entity
+@Table("kunden")
 public class Kunde {
+	
+	@Id
+	private UUID id;
+	
+	@NotNull
+	@Size(max = 50)
+	private String nachname;
+	
+	@NotNull
+	@Size(max = 50)
+	private String vorname;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "kunde")
+	private Set<Rechnung> rechnungen = new HashSet<>();
+	
+	
 }
